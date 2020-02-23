b135c93ec893dbc071c9624b65aab5f69766ab06
indraawagin <indraawagin@gmail.com>
2020-02-23 22:20:55 +0800

    Membuat koneksi ke database

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+<?php
+
+$database	= 'hapus_ala_gmail';
+$sumber		= 'mysql:dbname=' . $database . ';host=127.0.0.1';
+$user 		= 'root';
+$password 	= '';
+
+try {
+	$koneksi = new PDO($sumber, $user, $password);
+} catch (PDOException $e) {
+	echo 'Koneksi gagal : ' . $e->getMessage();
+}
